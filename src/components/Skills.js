3b966055cf585bc2585a1a1e8b0a2e89777e93f4
Sujal <sujal@pop-os.localdomain>
2023-05-17 20:22:00 +0530

    changes in Style

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -76,7 +76,7 @@ const Skills = () => {
   ];
   return (
     <div className="section3 absolute flex flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center ">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-[2rem] uppercase tracking-[20px] text-gray-500 text-2xl ">
         Skills
       </h3>
       <div className="grid grid-cols-4 gap-5">
